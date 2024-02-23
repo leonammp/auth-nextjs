c1221ccf652de0f06a3b250b8b973f62a731177d
Leonam Mendonça <leonam.pereira@finnet.com.br>
2024-02-23 10:23:58 -0300

    add register action

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,6 +12,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Invalid fields!" }
     }
+
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    if (existingUser) {
+        return { error: "Email already in use!"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    //TODO:  Send verification token email
 
     return { success: "Email sent!" }
 }
