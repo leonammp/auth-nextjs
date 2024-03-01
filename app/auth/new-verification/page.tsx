8be8a52ab0e97a0c3932d489143cf0b026574089
Leonam Mendonça <leonam.pereira@finnet.com.br>
2024-03-01 18:31:48 -0300

    add email verification page

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,9 @@
+import { NewVerificationFrom } from "@/components/auth/new-verification-form";
+
+const NewVerificationPage = () => {
+    return (
+        <NewVerificationFrom/>
+    );
+}
+
+export default NewVerificationPage;
